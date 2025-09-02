a1f39ed1b14e46d49cc7017ea1d7bc74842ffa5a
IT24101268 <it24101268@my.sliit.lk>
2025-09-02 10:16:28 +0530

    Add files via upload

---
+++ Lab Sheet 06/Lab06.R
@@ -0,0 +1,15 @@
+setwd("C:\\Users\\IT24101268\\Desktop\\Lab06")
+
+#Question 1
+#i.The distribution of X is a binomial distribution
+#ii.
+1-pbinom(46,50,0.85,lower.tail=TRUE)
+
+#Question 2
+#i.number of calls recieved by the call center for the hour
+
+#ii.the distribution of X is poisson
+
+#iii. P(X=15)
+dpois(15,lambda = 12)
+
